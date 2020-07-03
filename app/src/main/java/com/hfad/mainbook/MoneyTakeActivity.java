3a84ba0a362cbd5179ea759358720420b2ff9f05
sauravpatil1 <49922284+sauravpatil1@users.noreply.github.com>
2020-07-03 11:14:28 +0530

    Added give and take activity

--- app/src/main/java/com/hfad/mainbook/MoneyTakeActivity.java
+++ app/src/main/java/com/hfad/mainbook/MoneyTakeActivity.java
@@ -2,19 +2,12 @@ package com.hfad.mainbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MoneyTakeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onClickTake(View view) {
-    }
-
-    public void onClickGive(View view) {
+        setContentView(R.layout.activity_money_take);
     }
 }
